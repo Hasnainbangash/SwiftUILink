d08b5baaaca0f4eaa71bb6ed0a9a609162514550
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-07 16:48:25 +0500

    Added a simple link

--- SwiftUILink/ContentView.swift
+++ SwiftUILink/ContentView.swift
@@ -9,13 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        // Simple link with the title and with te destination
+        Link("Go to Apple", destination: URL(string: "https://apple.com")!).padding()
+        
     }
 }
 
