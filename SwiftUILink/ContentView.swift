484bdb49817967531232c883250cf9fe4fd8d283
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-07 16:57:01 +0500

    Added a custom link view

--- SwiftUILink/ContentView.swift
+++ SwiftUILink/ContentView.swift
@@ -33,6 +33,24 @@ struct ContentView: View {
             .buttonStyle(.plain)
             .padding()
             .border(.primary, width: 2)
+            
+            .padding()
+        
+        // Link View
+        Link(destination: URL(string: "https://apple.com")!) {
+            HStack(spacing: 16) {
+                Image(systemName: "apple.logo")
+                Text("Apple Store")
+            }
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .padding(.horizontal)
+            .background(
+                Capsule()
+                    .fill(Color.blue)
+            )
+        }
         
     }
 }
