c7bf5fd92a1c5e4dba5b0ffe1437370d14973f9d
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-07 16:36:38 +0500

    Initial Commit

---
+++ SwiftUILink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUILink
+//
+//  Created by Elexoft on 07/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUILink/SwiftUILinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILinkApp.swift
+//  SwiftUILink
+//
+//  Created by Elexoft on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
